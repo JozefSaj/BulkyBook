273fb208d9c99f3e3a55e03eb14eaa2e3c2064be
Jozef Šajtlava <jozef.sajtlava02@gmail.com>
2023-03-24 23:52:50 +0100

    sql server, connection string, Failed to load configuration

---
+++ WebApplication1/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) :base(options)
+        {
+        }
+        //creating category table in database
+        public DbSet<Category> Categories { get; set; } //it will create a category table with the name of categories with 4 columns we have wrote in Categoty.cs
+    }
+}
---
+++ WebApplication1/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Category
+    {
+        [Key] //primary key
+        public int Id { get; set; }
+        [Required] //name is a required property
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
