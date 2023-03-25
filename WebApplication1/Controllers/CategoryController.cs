acf04993f02b65fb9b19210a3bc9543ac9b6c802
Jozef Šajtlava <jozef.sajtlava02@gmail.com>
2023-03-25 15:57:15 +0100

    displayes category table successfuly

---
+++ WebApplication1/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
