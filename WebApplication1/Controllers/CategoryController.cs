866b57ddbf72f70bbaffd869d448e8d2c419414f
Jozef Šajtlava <jozef.sajtlava02@gmail.com>
2023-03-27 20:40:07 +0200

    edit category button added

--- WebApplication1/Controllers/CategoryController.cs
+++ WebApplication1/Controllers/CategoryController.cs
@@ -36,6 +36,35 @@ namespace WebApplication1.Controllers
             return RedirectToAction("Index");
             }   
             return View(obj);
-    }
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id == null) 
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost] //if it is a pasto action we need to write attribute httppost
+        [ValidateAntiForgeryToken] //this aswell
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges(); //goes to the database and save the changes
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
